f83064a34e2a530d768389d7a677781bcdc27bc0
IvoLord <oleksandr.kotovskyi@gmail.com>
2022-01-25 23:12:28 +0100

    update index header

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,7 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var navList = document.querySelector('.main-nav__list');
+var mediaWidth = window.matchMedia('(min-width: 1200px)');
 
 navList.classList.add('main-nav__list--closed');
 navToggle.classList.remove('main-nav__toggle--nojs');
@@ -13,7 +14,6 @@ navToggle.addEventListener('click', function () {
     navToggle.classList.add('main-nav__toggle--opened');
     navMain.classList.add('main-nav--opened');
 
-
   } else {
     navList.classList.add('main-nav__list--closed');
     navList.classList.remove('main-nav__list--opened');
@@ -22,3 +22,14 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+function windowCheck(e) {
+  if (e.matches && navList.classList.contains('main-nav__list--opened')) {
+    navMain.classList.remove('main-nav--opened');
+  }
+  else if (!e.matches && navList.classList.contains('main-nav__list--opened')) {
+    navMain.classList.add('main-nav--opened');
+  }
+}
+
+mediaWidth.addListener(windowCheck);
